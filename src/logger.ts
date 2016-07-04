d098266cc7145a1054d266e1b870e5da47852cfb
christo8989 <christo8989@gmail.com>
2016-07-03 17:15:27 -0700

    Initial commit

---
+++ src/logger.ts
@@ -0,0 +1,11 @@
+'use-strict';
+
+class Logger {
+    public log(...args: any[]) {
+        console.log(args);
+    }
+
+    public logVerbose(...args: any[]) {
+        console.log(args);
+    }
+}
